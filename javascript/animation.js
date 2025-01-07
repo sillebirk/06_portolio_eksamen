0ef96f51ab4499f666bc48567084d3c72d07e726
Sille Birk <sila0004@stud.kea.dk>
2025-01-07 22:10:38 +0100

    helt done

--- javascript/animation.js
+++ javascript/animation.js
@@ -3,24 +3,26 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
   console.log("sidenVises");
 
-  //tilføjer classen "pulse" til play-knappen
+  //   tilføjer classen "pulse" og event der fører hen til en funktion når man klikker
   document.querySelector("#bi").classList.add("pulse");
   document.querySelector("#bi").addEventListener("click", clickHandler);
 }
 
-//
+// Når der bliver klikket starter animationen "click", som er en class der er blevet tilføjet
 function clickHandler() {
   console.log("clickAnimation");
   document.querySelector("#bi").classList.add("forsvind");
+  //   lytter efter om animationen er færdig
   document.querySelector("#bi").addEventListener("animationend", clickReset);
 }
 
 function clickReset() {
   console.log("clickReset");
+  //    fjerner alle classes fra html elementet, force reflow (for at computeren når at se at classen er fjernet)
   document.querySelector("#bi").classList = "";
   document.querySelector("#bi").offsetLeft;
   document.querySelector("#bi").firstElementChild.offsetLeft;
-
+  //    tilføjer classen igen, som nu starter forfra
   document.querySelector("#bi").classList.add("pulse");
   document.querySelector("#bi").addEventListener("click", clickHandler);
 }
